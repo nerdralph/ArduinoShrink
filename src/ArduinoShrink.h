583dba5b15949265eaab81012176097e9b9f8e31
Nerd Ralph <ralphdoncaster@gmail.com>
2020-08-29 21:29:54 -0300

    rm != 16MHz error

--- src/ArduinoShrink.h
+++ src/ArduinoShrink.h
@@ -1,10 +1,6 @@
 // (c) Ralph Doncaster 2020
 // ArduinoShrink
 
-#if F_CPU != 16000000
-#error Only 16Mhz F_CPU supported
-#endif
-
 #include <util/delay.h>
 
 inline void delayMicroseconds(unsigned int us)
