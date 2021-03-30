1b8f3f632fa4517584785b8c339c4b7d7417dec0
U-parsons-PC\parsons <ralphdoncaster@gmail.com>
2021-03-30 18:32:50 -0300

    new millis ISR equivalent to Arduno

--- src/wiring.c
+++ src/wiring.c
@@ -3,33 +3,49 @@
 
 #include <Arduino.h>
 #include <util/delay.h>
+#include "as_common.h"
 
-// lightweight custom function call abi
-extern void delay_impl();
-void delay(uint32_t msec)
+// delays a specified number of microseconds - modified from picoCore
+// works for clock frequencies of 4Mhz and up
+void delayMicroseconds(uint16_t us)
 {
-    register uint32_t ms asm ("r24") = msec;
-    asm ( ".equ T0CNT_PER_MS, %0\n" 
-        ".global T0CNT_PER_MS\n"
-        // T0 prescaler is 64
-        :: "i" (F_CPU / 1000 / 64) );
-    asm volatile (
-        "rcall %x1\n" 
-        : "+r"(ms)
-        : "i"(delay_impl)
-        : "r18", "r19", "r20", "r21"
-    );
+    // if us is a compile-time constant result is accurate to 1 cycle
+    if (__builtin_constant_p(us)) {
+        _delay_us(us);
+        return;
+    }
+
+    // code below runs when us is not known at compile time
+    // uses inline asm to guarantee exact cycle counts
+    const float fMHz = (F_CPU/1000000.0);
+    delay1us:
+        // delay 1us per loop, less 4 cycles for overhead
+        _delay_us(1.0 - (4.0 / fMHz));
+        asm volatile ("sbiw %[us], 1" : [us]"+d"(us));
+    asm goto( "brne %l[delay1us]" :::: delay1us);
 }
 
-// avr-libc math.h does not declare gcc log2 builtin
-double log2(double);
-
-void init()
+void init(void)
 {
-    asm("sei");
+    // T0CNT_PER_MS needed for delay_impl
+    asm ( ".equ T0CNT_PER_MS, %0\n" 
+        ".global T0CNT_PER_MS\n"
+        :: "i" (F_CPU / 1000 / T0_PRESCALE) );
 
     // setup ADC with 250-500kHz clock
     uint8_t prescaler = log2(F_CPU/500000);
     ADCSRA = prescaler | (1 << ADEN);
+
+    TCCR0B = _BV(CS01) | _BV(CS00);     // enable T0, div64 prescale
+
+    asm("sei");                         // enable interrupts
+}
+
+// replace main() from Arduino core
+int main(void)
+{
+    init();
+    setup();
+    while(1) loop();
 }
 
